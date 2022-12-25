bcb0c9313c1fa698b646836e5de757585d56303f
atwanzireT <tatwanzire@gmail.com>
2022-12-26 00:24:17 +0300

    get one and delete

--- posts/routes.py
+++ posts/routes.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Response, HTTPException
 from . import database
 from pydantic import BaseModel
 from .database import cursor
 
 app = FastAPI()
 
+
 class Post(BaseModel):
-    title : str
-    content : str
-    is_published : bool = True
-    
+    title: str
+    content: str
+    is_published: bool = True
+
+
 @app.get('/posts')
 def get_post():
     cursor.execute("""SELECT * FROM posts""")
@@ -17,45 +19,54 @@ def get_post():
     print(posts)
     return {'data': posts}
 
-@app.post('/posts', status_code= status.HTTP_201_CREATED)
-def createpost(post:Post):
+
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+def createpost(post: Post):
     cursor.execute("""INSERT INTO posts(title, content, is_published) values (%s, %s, %s) RETURNING *
     """, (post.title, post.content, post.is_published))
     new_post = cursor.fetchone()
-    return {"data":new_post}
+    return {"data": new_post}
 
 
 @app.get("/posts/{id}")
-def get_post(id : int, response : Response):
-    post = find_post((id))
+def get_post(id: str):
+    cursor.execute("""SELECT FROM posts WHERE id = %s""", (str(id),))
+    post = cursor.fetchone()
     if not post:
-        raise (HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail={"message" : f"Post of id {id} was not found."}))
+        raise (HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+               detail={"message": f"Post of id {id} was not found."}))
         # response.status_code = status.HTTP_404_NOT_FOUND
         return {"message": f"Post of id {id} was not found."}
-    return {"Post Detail" : post}
+    return {"Post Detail": post}
+
 
 def find_post_index():
-    for i, p in enumerate (my_posts):
+    for i, p in enumerate(my_posts):
         if p['id'] == id:
             return i
 
-@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
-def delete_post(id):
-    index = find_post_index(id)
 
-    if index == None:
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    my_posts.pop(index)
-    return HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail={"message": f"Post of id {id} was not found."})
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    cursor.execute(
+        """DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    delete_post = cursor.fetchone()
+    cursor.commit()
+    if delete_post == None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail={
+                            "message": f"Post of id {id} was not found."})
 
-@app.update("/posts/{id}")
-def update_post(id : int, post :Post):
-    index = find_post_index(id)
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
 
-    if index == None:
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    
-    post_dict = post.dict()
-    post['id'] = id
-    my_posts[index] = post_dict
-    return {'message': post_dict}
+
+# @app.update("/posts/{id}")
+# def update_post(id: int, post: Post):
+#     index = find_post_index(id)
+
+#     if index == None:
+#         return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+#     post_dict = post.dict()
+#     post['id'] = id
+#     my_posts[index] = post_dict
+#     return {'message': post_dict}
