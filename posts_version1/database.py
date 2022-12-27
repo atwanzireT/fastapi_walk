4e0980732c6e2dd9766f403735b395326f603c61
atwanzireT <tatwanzire@gmail.com>
2022-12-27 17:07:02 +0300

    dependencies, sqlalchemy, models

---
+++ posts_version1/database.py
@@ -0,0 +1,13 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+try:
+    conn = psycopg2.connect(host = 'localhost', database = 'fastapi', user = 'postgres', 
+    password = 'bira', cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print("Database connected Successfully.")
+except Exception as error:
+    print("Database Failed to connect.")
+    print({"Error": error})
+    time.sleep(5)
---
+++ posts_version2/routes.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, status, Response, HTTPException
+from . import database
+from pydantic import BaseModel
+from .database import cursor, conn
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind = engine)
+
+app = FastAPI()
+
+# dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    is_published: bool = True
+
+
+@app.get('/posts')
+def get_post():
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    print(posts)
+    return {'data': posts}
+
+
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+def createpost(post: Post):
+    cursor.execute("""INSERT INTO posts(title, content, is_published) values (%s, %s, %s) RETURNING *
+    """, (post.title, post.content, post.is_published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
+
+
+@app.get("/posts/{id}")
+def get_post(id: str):
+    cursor.execute("""SELECT * from posts WHERE id = %s """, (str(id),))
+    post = cursor.fetchall()
+    return {"Post Detail": post}
+
+
+def find_post_index():
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    cursor.execute(
+        """DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    delete_post = cursor.fetchall()
+    conn.commit()
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
+
+
+# @app.update("/posts/{id}")
+# def update_post(id: int, post: Post):
+#     index = find_post_index(id)
+
+#     if index == None:
+#         return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+#     post_dict = post.dict()
+#     post['id'] = id
+#     my_posts[index] = post_dict
+#     return {'message': post_dict}
