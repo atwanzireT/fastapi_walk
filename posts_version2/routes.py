1a19499c119e468453a5c97731d0d973283e8404
atwanzireT <tatwanzire@gmail.com>
2023-01-02 01:46:52 +0300

    create and get all operations

--- posts_version2/routes.py
+++ posts_version2/routes.py
@@ -24,24 +24,37 @@ class Post(BaseModel):
     is_published: bool = True
 
 
-@app.get('/posts')
-def get_post():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    print(posts)
-    return {'data': posts}
+# @app.get('/posts')
+# def get_post():
+#     cursor.execute("""SELECT * FROM posts""")
+#     posts = cursor.fetchall()
+#     print(posts)
+#     return {'data': posts}
 
+@app.get('/posts')
+def get_post(db : Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+    
 @app.get('/sqlalchemy')
 def test_posts(db : Session = Depends(get_db)):
     return {"status":"success"}
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
-def createpost(post: Post):
-    cursor.execute("""INSERT INTO posts(title, content, is_published) values (%s, %s, %s) RETURNING *
-    """, (post.title, post.content, post.is_published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def createpost(post: Post, db : Session = Depends(get_db)):
+    new_post = models.Post(title = post.title, content = post.content, published = post.is_published)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data": new_post}
+
+# @app.post('/posts', status_code=status.HTTP_201_CREATED)
+# def createpost(post: Post):
+#     cursor.execute("""INSERT INTO posts(title, content, is_published) values (%s, %s, %s) RETURNING *
+#     """, (post.title, post.content, post.is_published))
+#     new_post = cursor.fetchone()
+#     conn.commit()
+#     return {"data": new_post}
 
 
 @app.get("/posts/{id}")
