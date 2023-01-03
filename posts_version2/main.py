9966ffceaa103445f67df05e4b1c99715b2bc3f7
atwanzireT <tatwanzire@gmail.com>
2023-01-04 01:06:59 +0300

    getting single user by id

--- posts_version2/main.py
+++ posts_version2/main.py
@@ -82,11 +82,17 @@ def create_user(user:UserCreate, db: Session = Depends(get_db)):
     # hash the password - user.password
     hashed_password = pwd_crypt.hash(user.password)
     user.password = hashed_password
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get("/user{id}", status_code=status.HTTP_200_OK)
+def get_user(id:int, db : Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).all()
+    return user
 
 # def find_post_index():
 #     for i, p in enumerate(my_posts):
