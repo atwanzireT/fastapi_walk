d95c3c352bd353175696f82899925ecac391c152
atwanzireT <tatwanzire@gmail.com>
2023-01-09 09:03:00 +0300

    fixing bugs

--- blogApi/core/Oauth2.py
+++ blogApi/core/Oauth2.py
@@ -19,14 +19,14 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data : dict):
     to_encode = data.copy()
     # Expriation Time
-    expire = datetime.now() + timedelta()
+    expire = datetime.utcnow.now() + timedelta()
     to_encode.update({"exp":expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def verify_access_token(token : str, credential_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id : str = payload.get("user_id")
 
         if id is None:
@@ -35,6 +35,8 @@ def verify_access_token(token : str, credential_exception):
         token_data = schemas.TokenData(id = id)
     except:
         raise credential_exception
+    
+    return token_data
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: session = Depends(database.get_db)):
